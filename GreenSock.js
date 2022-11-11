5e2330dee2f5bb6672d8e3815c9262004fab24bf
Coreanders <coreynregan@gmail.com>
2022-11-11 00:16:57 -0500

    page 2 animation

--- GreenSock.js
+++ GreenSock.js
@@ -1,3 +1,3 @@
 // Greensock animation tutorial https://www.youtube.com/watch?v=m6PDUIF24v4
 
-// gsap.from('#welcome', { duration: 1, y: '-200%', ease: 'elastic', opacity: 0 })
+gsap.from('#welcome', { duration: 0.5, y: '-200%', ease: 'bounce', opacity: 0 })
